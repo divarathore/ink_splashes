fd502e8210ac61defd71d6a4bf32d7ef1e965374
divarathore <rathd119@newschool.edu>
2021-04-13 21:02:54 +0530

    scale up

--- script2.js
+++ script2.js
@@ -58,86 +58,16 @@ function showSplashes() {
         document.querySelector(".game").appendChild(card);
       
         var splashImage = document.createElement("img");
-        splashImage.classList.add("front")
+        splashImage.classList.add("front");
+        card.classList.add("open");
         splashImage.src = splash.fields.images[0].url;
         card.appendChild(splashImage);
         // document.querySelector(".card-face").append(splashImage);
     });
   }
-  // var card = document.querySelectorAll('img');
-  // card.forEach((item) => {
 
-  //   item.addEventListener( 'mouseDown', function() {
-  //     console.log('clickeddd');
-  //   item.classList.toggle('.front');
-  //     // console.log('hello');
-  // });
-  // });
-
-// document.querySelector("body").onclick = function(){
-
-//   console.log('hello');
-//   document.querySelector("img").style.transform = "scale(2)";
-  // document.getElementsByClassName(".front").style.transform = 'scale(2)';
-  // document.getElementsByClassName(".front").style.transition = '0.8';
-  
-// }
-// document.addEventListener("body").onclick = function(){
-//   var element = document.querySelector(".front");
-//   element.classList.toggle("mystyle");
-//   console.log('img clicked')
-// }
-// var image =  document.querySelector('.front');
-
-// function mouseOver() {
-//     image.style.height="600px";
-// }
-
-// image.onclick = mouseOver;
-
-// document.querySelector("body").onclick = function(){
-//   var element = document.getElementsByClassName(".front");
-//   element.classList.toggle(".mystyle");
-// }
-
-// document.querySelector("body").onclick = function(){
-//   var element = document.getElementsByClassName(".front");
-
-//   if (element.classList) { 
-//     element.classList.toggle("mystyle");
-//   } else {
-//     var classes = element.className.split(" ");
-//     var i = classes.indexOf("mystyle");
-
-//     if (i >= 0) 
-//       classes.splice(i, 1);
-//     else 
-//       classes.push("mystyle");
-//       element.className = classes.join(" "); 
-//   }
-// }
-
-// var tet = document.getElementsByClassName("card-face");
-// tet[0].classList.toggle("front");
-// var togg = document.getElementsByClassName("front");
-
-// function toggl() {
-//   tet[0].classList.toggle("front");
-// }
-// document.addEventListener("click", toggl);
-// function toggl () {
-//   for (var i = 0; i < two.length; i++) {
-//     tet[i].classList.toggle("front");
-//   }
-// }
-
-// var test = document.getElementsByClassName('.card-face');
-// test.onclick = function myClickFunction() {
-//   console.log('hello');
-//   img.style.transform = scale(2);
-// }
-// function onClick(element) {
-//   document.getElementsByClassName("front").src = element.src;
-//   // splashImage.classList.add("mystyle")
-//   document.getElementsByClassName("front").style.transform = 'scale(2)';
-// }
+  document.querySelector('.game').addEventListener('click', (event) => {
+    if (event.target.tagName === 'IMG') {
+        event.target.classList.toggle('scale-up');
+    }
+});
